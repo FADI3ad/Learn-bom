71a4140d2342bf7b0b6d1609b8e18b2d8453fd3b
Foda <fadyemad687@gmail.com>
2025-03-29 22:31:06 +0200

    window , alert , this , sweetalert , confirm , prompt

---
+++ main.js
@@ -0,0 +1,35 @@
+// window.console.log(4);
+
+
+
+// window.alert("fady")
+// alert("fady");
+// this.alert("fadi");
+// Swal.fire({
+//     title: "Do you want to save the changes?",
+//     showDenyButton: true,
+//     showCancelButton: true,
+//     confirmButtonText: "Save",
+//     denyButtonText: `Don't save`
+//   }).then((result) => {
+
+//     if (result.isConfirmed) {
+//       Swal.fire("Saved!", "", "success");
+//     } else if (result.isDenied) {
+//       Swal.fire("Changes are not saved", "", "info");
+//     }
+//   });
+
+
+
+
+
+// let confmsg=confirm("Are You shoure yo want leav ? ");
+// console.log(confmsg);
+// let promptmsg=prompt("Are You shoure yo want leav ? " , "yes or no" );
+// console.log(promptmsg);
+
+
+
+
+
